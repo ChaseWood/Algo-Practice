a96c97ba2169b2c46ef30624ffa6ef4d3f5eba94
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-12-02 17:53:38 -0600

    practicing

--- PrintArray.js
+++ PrintArray.js
@@ -1,7 +1,20 @@
 //Write a function called `printArr` that will print the items of an array.
 
+// const printArr = (array) => {
+// 	array.map((e) => console.log(e));
+// };
+
 const printArr = (array) => {
-	array.map((e) => console.log(e));
+	newArr = '';
+	for (let i = 0; i < array.length; i++) {
+		newArr += `${array[i]},`;
+	}
+	return newArr;
 };
 
 console.log(printArr([1, 2, 3, 4, 5]));
+
+// Questions?
+// Is it possible that the array is empty? Should I account for this?
+// Will the array contain nested arrays and should I print those?
+// Should I console.log the characters or return a string with the characters?
---
+++ reverseAString.js
@@ -0,0 +1,26 @@
+// THERE ARE A FEW DIFFERENT WAYS TO REVERSE A STRING...
+// MAKING VARIABLES AND USING A FOR LOOP
+// USING THE REVERSE METHOD
+
+const reverse = (string) => {
+	// THIS METHOD GOES FORWARD OVER THE STRING WITH A FOR LOOP
+	// const solution = [];
+	// const newString = string.split('');
+	// for (let i = 0; i < string.length; i++) {
+	// 	solution.unshift(newString[i]);
+	// }
+	// const newSolution = solution.join('');
+	// return newSolution;
+	// THIS METHOD GOES BACKWARD WITH THE FOR LOOP
+	// CONCATING TO A NEW STRING
+	// var solution = '';
+	// for (let i = string.length - 1; i >= 0; i--) {
+	// 	solution += string[i];
+	// }
+	// return solution;
+	// THIS METHOD USES THE ARRAY METHODS SPLIT, REVERSE AND JOIN
+	// const solution = string.split('').reverse().join('');
+	// return solution;
+};
+
+console.log(reverse('chase'));
